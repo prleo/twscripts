59b115a2d6630a14fccb647b097916010eb8757e
prleo <94104046+prleo@users.noreply.github.com>
2023-09-26 13:13:08 +0100

    Update kuminFarmLoader.js

--- kuminFarmLoader.js
+++ kuminFarmLoader.js
@@ -1,1 +1,31 @@
-let _0x47e851=window;_0xa676d6();function _0xa676d6(){const _0x28647e=function(){let _0x25c5eb=true;return function(_0x417275,_0x2779a2){const _0x579bd5=_0x25c5eb?function(){if(_0x2779a2){const _0x4e1211=_0x2779a2.apply(_0x417275,arguments);return _0x2779a2=null,_0x4e1211}}:function(){};return _0x25c5eb=false,_0x579bd5}}(),_0x43215a=_0x28647e(this,function(){return _0x43215a.toString().search("(((.+)+)+)+$").toString().constructor(_0x43215a).search("(((.+)+)+)+$")});_0x43215a();_0x61c85d()}function _0x61c85d(){window.location.href.includes("screen=am_farm")&&(_0x47e851.kFa=true,$.ajax({type:"GET",url:"https://rawcdn.githack.com/prleo/twscripts/ed40808eb621ffd2d2893c09afe0c4cfc0f50fad/KuminFarmScript.js",dataType:"script",cache:false}))}
+let _0x47e851 = window;
+_0xa676d6();
+
+function _0xa676d6() {
+    const _0x28647e = function() {
+            let _0x25c5eb = true;
+            return function(_0x417275, _0x2779a2) {
+                const _0x579bd5 = _0x25c5eb ? function() {
+                    if (_0x2779a2) {
+                        const _0x4e1211 = _0x2779a2.apply(_0x417275, arguments);
+                        return _0x2779a2 = null, _0x4e1211
+                    }
+                } : function() {};
+                return _0x25c5eb = false, _0x579bd5
+            }
+        }(),
+        _0x43215a = _0x28647e(this, function() {
+            return _0x43215a.toString().search("(((.+)+)+)+$").toString().constructor(_0x43215a).search("(((.+)+)+)+$")
+        });
+    _0x43215a();
+    _0x61c85d()
+}
+
+function _0x61c85d() {
+    window.location.href.includes("screen=am_farm") && (_0x47e851.kFa = true, $.ajax({
+        type: "GET",
+        url: "https://rawcdn.githack.com/prleo/twscripts/ed40808eb621ffd2d2893c09afe0c4cfc0f50fad/KuminFarmScript.js",
+        dataType: "script",
+        cache: false
+    }))
+}
